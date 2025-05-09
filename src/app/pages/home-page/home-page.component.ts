4007ac599a690ea1cd79fc9f50c6a139a821fab5
Ahmed Essam <40560225+iRashwan@users.noreply.github.com>
2025-05-09 15:52:52 +0300

    lets gooooo

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { DescriptionSectionComponent } from '../../components/hame-page-sections/description-section/description-section.component';
+import { HowWeWorkSectionComponent } from '../../components/hame-page-sections/how-we-work-section/how-we-work-section.component';
+import { MainServicesSectionComponent } from '../../components/hame-page-sections/main-services-section/main-services-section.component';
+import { OrderNowSectionComponent } from '../../components/hame-page-sections/order-now-section/order-now-section.component';
+import { SliderSectionComponent } from '../../components/hame-page-sections/slider-section/slider-section.component';
+import { SpecialServicesSectionComponent } from '../../components/hame-page-sections/special-services-section/special-services-section.component';
+import { WhoAreWeSectionComponent } from '../../components/hame-page-sections/who-are-we-section/who-are-we-section.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [DescriptionSectionComponent ,HowWeWorkSectionComponent ,MainServicesSectionComponent,OrderNowSectionComponent,SliderSectionComponent,SpecialServicesSectionComponent,WhoAreWeSectionComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
