dc48dcb432d2d3873bfd6cb41560c1cced681715
ahmedaladdin6 <ahmedaladdin6@gmail.com>
2025-07-02 01:40:12 +0300

    fixed profile image update

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,20 +1,20 @@
-import { Component, computed, Inject, inject, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { Router, RouterModule, NavigationEnd } from '@angular/router';
-import { TranslateModule, TranslatePipe } from '@ngx-translate/core';
-import { LanguageService } from '../../services/language.service';
-import { ToasterService } from '../../services/toaster.service';
-import { DOCUMENT, NgFor, NgIf } from '@angular/common';
-import { InputGroup } from 'primeng/inputgroup';
-import { InputTextModule } from 'primeng/inputtext';
-import { IconFieldModule } from 'primeng/iconfield';
-import { InputIconModule } from 'primeng/inputicon';
-import { MenubarModule } from 'primeng/menubar';
-import { MenuItem } from 'primeng/api';
-import { filter } from 'rxjs/operators';
-import { LoginSignalUserDataService } from '../../services/login-signal-user-data.service';
-import { ApiService } from '../../services/api.service';
-import { NotificationComponent } from '../../pages/notification/notification.component';
+import { Component, computed, Inject, inject, OnInit } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { Router, RouterModule, NavigationEnd } from "@angular/router";
+import { TranslateModule, TranslatePipe } from "@ngx-translate/core";
+import { LanguageService } from "../../services/language.service";
+import { ToasterService } from "../../services/toaster.service";
+import { DOCUMENT, NgFor, NgIf } from "@angular/common";
+import { InputGroup } from "primeng/inputgroup";
+import { InputTextModule } from "primeng/inputtext";
+import { IconFieldModule } from "primeng/iconfield";
+import { InputIconModule } from "primeng/inputicon";
+import { MenubarModule } from "primeng/menubar";
+import { MenuItem } from "primeng/api";
+import { filter } from "rxjs/operators";
+import { LoginSignalUserDataService } from "../../services/login-signal-user-data.service";
+import { ApiService } from "../../services/api.service";
+import { NotificationComponent } from "../../pages/notification/notification.component";
 
 interface User {
   username: string;
@@ -23,53 +23,75 @@ interface User {
   name: string | null;
 }
 @Component({
-  selector: 'app-navbar',
+  selector: "app-navbar",
   standalone: true,
-  imports: [TranslateModule, NgIf,NotificationComponent, TranslatePipe, RouterModule, MenubarModule, IconFieldModule, InputIconModule, FormsModule, InputGroup, InputTextModule, NgFor],
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  imports: [
+    TranslateModule,
+    NgIf,
+    NotificationComponent,
+    TranslatePipe,
+    RouterModule,
+    MenubarModule,
+    IconFieldModule,
+    InputIconModule,
+    FormsModule,
+    InputGroup,
+    InputTextModule,
+    NgFor,
+  ],
+  templateUrl: "./navbar.component.html",
+  styleUrl: "./navbar.component.scss",
 })
 export class NavbarComponent implements OnInit {
   // hasPermission: boolean = false;
-  localStorage=localStorage.getItem('token')
+  profileImg = "assets/img/profile-circle.svg";
+  localStorage = localStorage.getItem("token");
   langOptions = [
-    { name: 'English', code: 'en', icon: 'assets/images/icons/en-lang.png' },
-    { name: 'العربية', code: 'ar', icon: 'assets/images/icons/ar-lang.png' },
+    { name: "English", code: "en", icon: "assets/images/icons/en-lang.png" },
+    { name: "العربية", code: "ar", icon: "assets/images/icons/ar-lang.png" },
   ];
 
-  selectedLang: string = localStorage.getItem('lang') || 'ar';
+  selectedLang: string = localStorage.getItem("lang") || "ar";
   languageService = inject(LanguageService);
   toaster = inject(ToasterService);
-  currentLang = 'ar';
+  currentLang = "ar";
   items: MenuItem[] | undefined = [];
-  activeRoute: string = '';
+  activeRoute: string = "";
   authService = inject(LoginSignalUserDataService);
   api = inject(ApiService);
   // ✅ Move computed() outside ngOnInit()
   user = computed(() => this.authService.user());
 
-  constructor(@Inject(DOCUMENT) private document: Document, private router: Router) {
-    this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd)
-    ).subscribe((event: any) => {
-      this.activeRoute = event.url;
-    });
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private router: Router
+  ) {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        this.activeRoute = event.url;
+        this.profileImg =
+          localStorage.getItem("img") ?? "assets/img/profile-circle.svg";
+      });
   }
 
   ngOnInit(): void {
-
-    console.log('User Signal:', this.user()); // ✅ Access computed() as a function
+    console.log("User Signal:", this.user()); // ✅ Access computed() as a function
     this.initAppTranslation();
     // this.checkPermission();
-    this.languageService.translationService.onLangChange.subscribe((lang: any) => {
-      this.updateMenuItems();
-      this.selectedLang = lang.lang
-    });
+    this.languageService.translationService.onLangChange.subscribe(
+      (lang: any) => {
+        this.updateMenuItems();
+        this.selectedLang = lang.lang;
+      }
+    );
     this.updateMenuItems();
   }
 
   get languageToggleText(): string {
-    return this.selectedLang.toUpperCase() === 'AR' ? 'NAVBAR.ENGLISH' : 'NAVBAR.ARABIC';
+    return this.selectedLang.toUpperCase() === "AR"
+      ? "NAVBAR.ENGLISH"
+      : "NAVBAR.ARABIC";
   }
 
   // checkPermission() {
@@ -80,11 +102,24 @@ export class NavbarComponent implements OnInit {
 
   updateMenuItems() {
     this.items = [
-      { label: this.languageService.translate('NAVBAR.HOME'), routerLink: '/', routerLinkActiveOptions: { exact: true } },
-      { label: this.languageService.translate('NAVBAR.CONTACT'), routerLink: '/aboutus' },
+      {
+        label: this.languageService.translate("NAVBAR.HOME"),
+        routerLink: "/",
+        routerLinkActiveOptions: { exact: true },
+      },
+      {
+        label: this.languageService.translate("NAVBAR.CONTACT"),
+        routerLink: "/aboutus",
+      },
       // { label: this.languageService.translate('NAVBAR.DISCOUNTS'), routerLink: '/discounts' },
-      { label: this.languageService.translate('NAVBAR.STORES'), routerLink: '/traders_list' },
-      { label: this.languageService.translate('NAVBAR.BECOME_TRADER'), routerLink: '/become_trader' },
+      {
+        label: this.languageService.translate("NAVBAR.STORES"),
+        routerLink: "/traders_list",
+      },
+      {
+        label: this.languageService.translate("NAVBAR.BECOME_TRADER"),
+        routerLink: "/become_trader",
+      },
       // { label: this.languageService.translate('NAVBAR.ARTICLES'), routerLink: '/articles' },
       // {
       //   label: this.languageService.translate('NAVBAR.PRODUCTS'),
@@ -97,7 +132,6 @@ export class NavbarComponent implements OnInit {
     ];
   }
 
-
   public initAppTranslation() {
     this.languageService.changeAppDirection(this.selectedLang);
     this.languageService.changeHtmlLang(this.selectedLang);
@@ -105,19 +139,22 @@ export class NavbarComponent implements OnInit {
   }
 
   toggleLanguage() {
-    this.selectedLang = this.selectedLang === 'en' ? 'ar' : 'en';
+    this.selectedLang = this.selectedLang === "en" ? "ar" : "en";
     this.currentLang = this.selectedLang;
 
-    localStorage.setItem('lang', this.selectedLang);
+    localStorage.setItem("lang", this.selectedLang);
 
     this.languageService.change(this.selectedLang);
     this.languageService.use(this.selectedLang);
     this.languageService.changeAppDirection(this.selectedLang);
     this.languageService.changeHtmlLang(this.selectedLang);
 
-    this.document.body.dir = this.selectedLang === 'ar' ? 'rtl' : 'ltr';
-    document.documentElement.setAttribute('lang', this.selectedLang);
-    document.documentElement.setAttribute('dir', this.selectedLang === 'ar' ? 'rtl' : 'ltr');
+    this.document.body.dir = this.selectedLang === "ar" ? "rtl" : "ltr";
+    document.documentElement.setAttribute("lang", this.selectedLang);
+    document.documentElement.setAttribute(
+      "dir",
+      this.selectedLang === "ar" ? "rtl" : "ltr"
+    );
 
     this.updateMenuItems();
   }
@@ -126,10 +163,9 @@ export class NavbarComponent implements OnInit {
     localStorage.clear();
     this.authService.logout();
     // this.hasPermission=false;
-    this.router.navigate(['/']).then(()=>{
-      this.router.navigateByUrl('/home');
-      window.location.reload()
-    })
+    this.router.navigate(["/"]).then(() => {
+      this.router.navigateByUrl("/home");
+      window.location.reload();
+    });
   }
-
 }
