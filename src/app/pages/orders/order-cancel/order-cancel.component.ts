13be053f23c75244ba20aa029c290973769813ae
Ahmed Essam <40560225+iRashwan@users.noreply.github.com>
2025-07-24 02:27:36 +0300

    fix cancel reasons issue

--- src/app/pages/orders/order-cancel/order-cancel.component.ts
+++ src/app/pages/orders/order-cancel/order-cancel.component.ts
@@ -4,11 +4,13 @@ import { RadioButtonModule } from 'primeng/radiobutton';
 import { ApiService } from '../../../services/api.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { DynamicDialogRef } from 'primeng/dynamicdialog';
-import { TranslatePipe } from '@ngx-translate/core';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
+import { SelectModule } from 'primeng/select';
+
 @Component({
   selector: 'app-order-cancel',
   standalone: true,
-  imports: [RadioButtonModule, FormsModule,TranslatePipe],
+  imports: [RadioButtonModule, FormsModule , SelectModule , TranslatePipe],
   providers: [DynamicDialogRef],
   templateUrl: './order-cancel.component.html',
   styleUrl: './order-cancel.component.scss'
@@ -17,16 +19,41 @@ export class OrderCancelComponent {
   @Output() confirmOrder: any = new EventEmitter();
   checked = 0;
   orderCancelationNotes = '';
+  cancelReasonsList: any;
   private apiServices = inject(ApiService);
   private route = inject(ActivatedRoute);
   private router =inject(Router)
   private dialogRef = inject(DynamicDialogRef);
+  private translateService = inject(TranslateService);
+  selectedReasonId: any;
+
+
+ngOnInit(): void {
+  this.translateService.onLangChange.subscribe(() => {
+    this.getCancelReasons(); // reload reasons with correct labels
+  });
+  this.getCancelReasons();
+}
+
+getCancelReasons() {
+  const currentLang = this.translateService.currentLang || 'ar'; // default to Arabic
+
+  this.apiServices.get('CancelReason/GetAll').subscribe((res: any) => {
+    if (res?.data) {
+      this.cancelReasonsList = res.data
+        .map((item: any) => ({
+          label: currentLang === 'ar' ? item.arName : item.enName,
+          value: item.reasonId,
+        }));
+    }
+  });
+}
 
   onConfirm() {
     this.apiServices
       .put('Order/CancelOrder', {
         orderId: Number(this.route.snapshot.paramMap.get('id')),
-        cancelReasonId: this.checked,
+        cancelReasonId: this.selectedReasonId.value,
         orderCancelationNotes: this.orderCancelationNotes,
       })
       .subscribe((res) => {
