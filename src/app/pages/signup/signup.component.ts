c10614f7d3dfb552c31b69db8a050b6515934347
Youssef Amr <yamrrushdy@gmail.com>
2026-01-23 19:20:07 +0200

    remove logout and make otp closable

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,243 +1,243 @@
-import { Component, Inject, inject } from "@angular/core";
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from "@angular/forms";
-import { ApiService } from "../../services/api.service";
-import { PasswordModule } from "primeng/password";
-import { InputTextModule } from "primeng/inputtext";
-import { DOCUMENT, NgIf } from "@angular/common";
-import { TranslatePipe, TranslateService } from "@ngx-translate/core";
-import { ToasterService } from "../../services/toaster.service";
-import { LanguageService } from "../../services/language.service";
-import { Router, RouterModule } from "@angular/router";
-import { ModalComponent } from "../../components/modal/modal.component";
-import { OtpModalComponent } from "../../components/otp-modal/otp-modal.component";
-import { CheckboxModule } from "primeng/checkbox";
-import { CarouselModule } from "primeng/carousel";
+// import { Component, Inject, inject } from "@angular/core";
+// import {
+//   FormBuilder,
+//   FormControl,
+//   FormGroup,
+//   ReactiveFormsModule,
+//   Validators,
+// } from "@angular/forms";
+// import { ApiService } from "../../services/api.service";
+// import { PasswordModule } from "primeng/password";
+// import { InputTextModule } from "primeng/inputtext";
+// import { DOCUMENT, NgIf } from "@angular/common";
+// import { TranslatePipe, TranslateService } from "@ngx-translate/core";
+// import { ToasterService } from "../../services/toaster.service";
+// import { LanguageService } from "../../services/language.service";
+// import { Router, RouterModule } from "@angular/router";
+// import { ModalComponent } from "../../components/modal/modal.component";
+// import { OtpModalComponent } from "../../components/otp-modal/otp-modal.component";
+// import { CheckboxModule } from "primeng/checkbox";
+// import { CarouselModule } from "primeng/carousel";
 
-@Component({
-  selector: "app-signup",
-  standalone: true,
-  imports: [
-    NgIf,
-    TranslatePipe,
-    CarouselModule,
-    ReactiveFormsModule,
-    CheckboxModule,
-    RouterModule,
-    PasswordModule,
-    InputTextModule,
-    OtpModalComponent,
-  ],
-  templateUrl: "./signup.component.html",
-  styleUrl: "./signup.component.scss",
-})
-export class SignupComponent {
-  signupForm: FormGroup;
-  toaster = inject(ToasterService);
-  otpValue: string = "";
-  mobileNumber: string = "";
-  openOtpModal: boolean = false;
-  languageService = inject(LanguageService);
-  currentLang = "en";
-  selectedLang: string = localStorage.getItem("lang") || "en";
+// @Component({
+//   selector: "app-signup",
+//   standalone: true,
+//   imports: [
+//     NgIf,
+//     TranslatePipe,
+//     CarouselModule,
+//     ReactiveFormsModule,
+//     CheckboxModule,
+//     RouterModule,
+//     PasswordModule,
+//     InputTextModule,
+//     OtpModalComponent,
+//   ],
+//   templateUrl: "./signup.component.html",
+//   styleUrl: "./signup.component.scss",
+// })
+// export class SignupComponent {
+//   signupForm: FormGroup;
+//   toaster = inject(ToasterService);
+//   otpValue: string = "";
+//   mobileNumber: string = "";
+//   openOtpModal: boolean = false;
+//   languageService = inject(LanguageService);
+//   currentLang = "en";
+//   selectedLang: string = localStorage.getItem("lang") || "en";
 
-  carouselImages: string[] = [
-    "assets/images/backgrounds/register-slider1.svg",
-    "assets/images/backgrounds/register-slider2.svg",
-    "assets/images/backgrounds/register-slider3.svg",
-  ];
+//   carouselImages: string[] = [
+//     "assets/images/backgrounds/register-slider1.svg",
+//     "assets/images/backgrounds/register-slider2.svg",
+//     "assets/images/backgrounds/register-slider3.svg",
+//   ];
 
-  constructor(
-    private fb: FormBuilder,
-    @Inject(DOCUMENT) private document: Document,
-    private api: ApiService,
-    private translate: TranslateService,
-    private router: Router
-  ) {
-    this.signupForm = this.fb.group(
-      {
-        firstName: [
-          "",
-          {
-            validators: [Validators.required, Validators.minLength(3)],
-          },
-        ],
-        lastName: [
-          "",
-          {
-            validators: [Validators.required, Validators.minLength(3)],
-          },
-        ],
-        userName: [
-          "",
-          {
-            validators: [Validators.required, Validators.minLength(3)],
-          },
-        ],
-        email: ["", [Validators.required, Validators.email]],
-        country: ["SA", Validators.required],
-        mobileNumber: [
-          "",
-          [
-            Validators.required,
-            this.mobileValidator.bind(this)
-          ],
-        ],
-        genderId: [1, Validators.required],
-      },
-    );
+//   constructor(
+//     private fb: FormBuilder,
+//     @Inject(DOCUMENT) private document: Document,
+//     private api: ApiService,
+//     private translate: TranslateService,
+//     private router: Router
+//   ) {
+//     this.signupForm = this.fb.group(
+//       {
+//         firstName: [
+//           "",
+//           {
+//             validators: [Validators.required, Validators.minLength(3)],
+//           },
+//         ],
+//         lastName: [
+//           "",
+//           {
+//             validators: [Validators.required, Validators.minLength(3)],
+//           },
+//         ],
+//         userName: [
+//           "",
+//           {
+//             validators: [Validators.required, Validators.minLength(3)],
+//           },
+//         ],
+//         email: ["", [Validators.required, Validators.email]],
+//         country: ["SA", Validators.required],
+//         mobileNumber: [
+//           "",
+//           [
+//             Validators.required,
+//             this.mobileValidator.bind(this)
+//           ],
+//         ],
+//         genderId: [1, Validators.required],
+//       },
+//     );
 
-    // Subscribe to country changes to update mobile validation
-    this.signupForm.get('country')?.valueChanges.subscribe(() => {
-      this.signupForm.get('mobileNumber')?.updateValueAndValidity();
-    });
+//     // Subscribe to country changes to update mobile validation
+//     this.signupForm.get('country')?.valueChanges.subscribe(() => {
+//       this.signupForm.get('mobileNumber')?.updateValueAndValidity();
+//     });
 
-    this.translate.setDefaultLang("en");
-    this.translate.use("en"); // You can change this dynamically
-  }
+//     this.translate.setDefaultLang("en");
+//     this.translate.use("en"); // You can change this dynamically
+//   }
 
-  ngOnInit(): void {
-    this.initAppTranslation();
-  }
-  onSubmit() {
-    if (this.signupForm.valid) {
-      const formValue = { ...this.signupForm.value };
+//   ngOnInit(): void {
+//     this.initAppTranslation();
+//   }
+//   onSubmit() {
+//     if (this.signupForm.valid) {
+//       const formValue = { ...this.signupForm.value };
       
-      // Ensure mobile number starts with 0
-      if (formValue.mobileNumber && !formValue.mobileNumber.startsWith('0')) {
-        formValue.mobileNumber = '0' + formValue.mobileNumber;
-      }
+//       // Ensure mobile number starts with 0
+//       if (formValue.mobileNumber && !formValue.mobileNumber.startsWith('0')) {
+//         formValue.mobileNumber = '0' + formValue.mobileNumber;
+//       }
 
-      const signupPayload = {
-        userId: 0,
-        roleId: 1,
-        isActive: true,
-        ...formValue,
-      };
+//       const signupPayload = {
+//         userId: 0,
+//         roleId: 1,
+//         isActive: true,
+//         ...formValue,
+//       };
 
-      this.api
-        .post("Authentication/Register", signupPayload)
-        .subscribe((res: any) => {
-          this.toaster.successToaster("تم إنشاء الحساب بنجاح");
-          this.router.navigate(["/auth/login"]);
-        });
-    } else {
-      this.toaster.errorToaster("يرجى إكمال جميع الحقول");
-    }
-  }
+//       this.api
+//         .post("Authentication/Register", signupPayload)
+//         .subscribe((res: any) => {
+//           this.toaster.successToaster("تم إنشاء الحساب بنجاح");
+//           this.router.navigate(["/auth/login"]);
+//         });
+//     } else {
+//       this.toaster.errorToaster("يرجى إكمال جميع الحقول");
+//     }
+//   }
 
-  toggleLanguage() {
-    this.selectedLang = this.selectedLang === "en" ? "ar" : "en";
-    this.currentLang = this.selectedLang;
-    this.languageService.change(this.selectedLang);
+//   toggleLanguage() {
+//     this.selectedLang = this.selectedLang === "en" ? "ar" : "en";
+//     this.currentLang = this.selectedLang;
+//     this.languageService.change(this.selectedLang);
 
-    this.document.body.dir = this.selectedLang === "ar" ? "rtl" : "ltr";
-    document.documentElement.setAttribute("lang", this.selectedLang);
-    document.documentElement.setAttribute(
-      "dir",
-      this.selectedLang === "ar" ? "rtl" : "ltr"
-    );
-  }
+//     this.document.body.dir = this.selectedLang === "ar" ? "rtl" : "ltr";
+//     document.documentElement.setAttribute("lang", this.selectedLang);
+//     document.documentElement.setAttribute(
+//       "dir",
+//       this.selectedLang === "ar" ? "rtl" : "ltr"
+//     );
+//   }
 
-  public initAppTranslation() {
-    this.languageService.changeAppDirection(this.selectedLang);
-    this.languageService.changeHtmlLang(this.selectedLang);
-    this.languageService.use(this.selectedLang);
-  }
+//   public initAppTranslation() {
+//     this.languageService.changeAppDirection(this.selectedLang);
+//     this.languageService.changeHtmlLang(this.selectedLang);
+//     this.languageService.use(this.selectedLang);
+//   }
 
-  onLogin(loginfrom: any) {
-    this.api.login(loginfrom).subscribe((res: any) => {
-      this.mobileNumber = res.mobilePhone;
-      this.openOtpModal = res.status;
-      if (!res.status) {
-        localStorage.removeItem("token");
-        this.toaster.errorToaster(res.message);
-      }
-    });
-  }
+//   onLogin(loginfrom: any) {
+//     this.api.login(loginfrom).subscribe((res: any) => {
+//       this.mobileNumber = res.mobilePhone;
+//       this.openOtpModal = res.status;
+//       if (!res.status) {
+//         localStorage.removeItem("token");
+//         this.toaster.errorToaster(res.message);
+//       }
+//     });
+//   }
 
-  getOtpValue(e: any) {
-    let otpObject = {
-      mobile: this.mobileNumber,
-      otpCode: e.otpValue,
-    };
-    this.api
-      .post("Authentication/VerfiyOtp", otpObject)
-      .subscribe((data: any) => {
-        console.log(data.data);
-        if (data.message == "Otp Is Not Valid") {
-          this.toaster.errorToaster(data.message);
-        } else {
-          let dataUser: any = {
-            img: data.data.imgSrc,
-            id: data.data.userId,
-            gender: data.data.gender,
-          };
-          localStorage.setItem("userData", JSON.stringify(dataUser));
-          localStorage.setItem("userId", JSON.stringify(dataUser.id));
-          localStorage.setItem("token", data.data.accessToken);
-          this.router.navigate(["/home"]);
-        }
-      });
-  }
+//   getOtpValue(e: any) {
+//     let otpObject = {
+//       mobile: this.mobileNumber,
+//       otpCode: e.otpValue,
+//     };
+//     this.api
+//       .post("Authentication/VerfiyOtp", otpObject)
+//       .subscribe((data: any) => {
+//         console.log(data.data);
+//         if (data.message == "Otp Is Not Valid") {
+//           this.toaster.errorToaster(data.message);
+//         } else {
+//           let dataUser: any = {
+//             img: data.data.imgSrc,
+//             id: data.data.userId,
+//             gender: data.data.gender,
+//           };
+//           localStorage.setItem("userData", JSON.stringify(dataUser));
+//           localStorage.setItem("userId", JSON.stringify(dataUser.id));
+//           localStorage.setItem("token", data.data.accessToken);
+//           this.router.navigate(["/home"]);
+//         }
+//       });
+//   }
 
-  resendOtp(e: any) {
-    this.onSubmit();
-  }
+//   resendOtp(e: any) {
+//     this.onSubmit();
+//   }
 
-  // Custom mobile validator
-  mobileValidator(control: FormControl) {
-    const country = this.signupForm?.get('country')?.value;
-    const mobileNumber = control.value;
+//   // Custom mobile validator
+//   mobileValidator(control: FormControl) {
+//     const country = this.signupForm?.get('country')?.value;
+//     const mobileNumber = control.value;
 
-    if (!mobileNumber) {
-      return null; // Let required validator handle empty values
-    }
+//     if (!mobileNumber) {
+//       return null; // Let required validator handle empty values
+//     }
 
-    if (country === 'SA') {
-      // Saudi mobile: 9 digits starting with 5
-      const saudiPattern = /^5\d{8}$/;
-      return saudiPattern.test(mobileNumber) ? null : { mobileValidator: true };
-    } else if (country === 'OM') {
-      // Omani mobile: 8 digits starting with 7, 9, or 2
-      const omaniPattern = /^[792]\d{7}$/;
-      return omaniPattern.test(mobileNumber) ? null : { mobileValidator: true };
-    }
+//     if (country === 'SA') {
+//       // Saudi mobile: 9 digits starting with 5
+//       const saudiPattern = /^5\d{8}$/;
+//       return saudiPattern.test(mobileNumber) ? null : { mobileValidator: true };
+//     } else if (country === 'OM') {
+//       // Omani mobile: 8 digits starting with 7, 9, or 2
+//       const omaniPattern = /^[792]\d{7}$/;
+//       return omaniPattern.test(mobileNumber) ? null : { mobileValidator: true };
+//     }
 
-    return { mobileValidator: true };
-  }
+//     return { mobileValidator: true };
+//   }
 
-  // Get country code for display
-  getCountryCode(): string {
-    const country = this.signupForm?.get('country')?.value;
-    return country === 'SA' ? '+966' : '+968';
-  }
+//   // Get country code for display
+//   getCountryCode(): string {
+//     const country = this.signupForm?.get('country')?.value;
+//     return country === 'SA' ? '+966' : '+968';
+//   }
 
-  // Get mobile placeholder based on country
-  getMobilePlaceholder(): string {
-    const country = this.signupForm?.get('country')?.value;
-    if (country === 'SA') {
-      return '5xxxxxxxx';
-    } else if (country === 'OM') {
-      return '7xxxxxxx / 9xxxxxxx / 2xxxxxxx';
-    }
-    return '';
-  }
+//   // Get mobile placeholder based on country
+//   getMobilePlaceholder(): string {
+//     const country = this.signupForm?.get('country')?.value;
+//     if (country === 'SA') {
+//       return '5xxxxxxxx';
+//     } else if (country === 'OM') {
+//       return '7xxxxxxx / 9xxxxxxx / 2xxxxxxx';
+//     }
+//     return '';
+//   }
 
-  // Get mobile error message based on country
-  getMobileErrorMessage(): string {
-    const country = this.signupForm?.get('country')?.value;
-    if (country === 'SA') {
-      return this.translate.instant('errors.saudiMobile');
-    } else if (country === 'OM') {
-      return this.translate.instant('errors.omaniMobile');
-    }
-    return this.translate.instant('errors.mobileNumber');
-  }
-}
+//   // Get mobile error message based on country
+//   getMobileErrorMessage(): string {
+//     const country = this.signupForm?.get('country')?.value;
+//     if (country === 'SA') {
+//       return this.translate.instant('errors.saudiMobile');
+//     } else if (country === 'OM') {
+//       return this.translate.instant('errors.omaniMobile');
+//     }
+//     return this.translate.instant('errors.mobileNumber');
+//   }
+// }
 
